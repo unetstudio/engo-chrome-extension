8e94c7c6e29dc8706ba5b37ca7e2d6fbc38ae362
Duc Dao <huuduc.uneti@gmail.com>
2019-11-10 22:39:25 +0700

    update select all

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,5 @@
+let createURL = 'http://engo.pro/words/create?q='
+
 document.addEventListener('DOMContentLoaded', function () {
     var links = document.getElementsByClassName("link");
     for (var i = 0; i < links.length; i++) {
@@ -34,17 +36,42 @@ function getSelectedText(callback) {
     });
 }
 
+function getAllText(callback) {
+    chrome.tabs.executeScript({
+        code: "document.body.innerText"
+    }, function (text) {
+        callback(text)
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var createSelect = document.getElementById('create-select');
+    var createAll = document.getElementById('create-all');
+
     createSelect.onclick = function () {
         getCurrentTabUrl(function (url) {
             var host = new URL(url);
             url = host.hostname;
-            var createURL = "http://engo.pro/words/create?q=";
 
             getSelectedText(function (selectedText) {
                 if (selectedText) {
                     createURL += encodeURI(selectedText)
+                }
+
+                createURL += "&ref=" + encodeURI(url);
+                chrome.tabs.create({"url": createURL});
+            })
+        })
+    };
+
+    createAll.onclick = function () {
+        getCurrentTabUrl(function (url) {
+            var host = new URL(url);
+            url = host.hostname;
+
+            getAllText(function (allText) {
+                if (allText) {
+                    createURL += encodeURI(allText)
                 }
 
                 createURL += "&ref=" + encodeURI(url);
