a00745ebb4a1791939a2cf7bbc8f877cd846312f
Duc Dao <huuduc.uneti@gmail.com>
2019-12-31 15:54:37 +0700

    clean browser action

--- src/background/background.js
+++ src/background/background.js
@@ -2,21 +2,23 @@
  * Context menu
  * @type {string}
  */
-let createURL = ENGOPRO.URL + "words/create?q=";
 
 chrome.contextMenus.create({
     "title": ENGOPRO.NAME,
     "contexts": ["page", "selection"],
     "onclick": function (e) {
-        let url = e.pageUrl;
+        const CREATE_URL = ENGOPRO.URL + "words/create?q=";
+        const url = e.pageUrl;
+        const host = new URL(url);
+        const site = host.hostname;
+
+        let createURL = CREATE_URL
         let selectionText = e.selectionText
         if (selectionText) {
             createURL += encodeURI(selectionText)
         }
 
-        createURL += "&ref=" + encodeURI(url);
+        createURL += "&ref=" + encodeURI(site);
         chrome.tabs.create({"url": createURL});
-        url = ''
-        selectionText = ''
     }
 });
--- src/browser_action/inject.js
+++ src/browser_action/inject.js
@@ -1,42 +1,6 @@
-/**
- * Browser action
- * @type {string}
- */
-let createURL = ENGOPRO.URL + "words/create?q=";
 
 /**
- * getCurrentTabUrl
- * @param callback
- */
-function getCurrentTabUrl(callback) {
-    let queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-
-    chrome.tabs.query(queryInfo, function (tabs) {
-        let tab = tabs[0];
-        let url = tab.url;
-        callback(url);
-    });
-}
-
-/**
- * getSelectionText
- * @param callback
- */
-function getSelectionText(callback) {
-    chrome.tabs.executeScript({
-        code: "window.getSelection().toString();"
-    }, function (selection) {
-        if (selection !== null && selection !== undefined && selection.length) {
-            callback(selection[0])
-        }
-    });
-}
-
-/**
- * Handle link lick
+ * Handle link click
  */
 function actionLink() {
     let links = document.getElementsByClassName("link");
@@ -54,35 +18,9 @@ function actionLink() {
 }
 
 /**
- * Trigger when click
- */
-function actionClick() {
-    let createSelect = document.getElementById('engopro-select');
-    createSelect.onclick = function () {
-        getCurrentTabUrl(function (url) {
-            let host = new URL(url);
-            url = host.hostname;
-
-            getSelectionText(function (selectionText) {
-                if (selectionText) {
-                    createURL += encodeURI(selectionText)
-                    createURL += "&ref=" + encodeURI(url);
-                    chrome.tabs.create({"url": createURL});
-                    url = ''
-                    selectionText = ''
-                } else {
-                    alert('Không có văn bản nào được chọn!')
-                }
-            })
-        })
-    };
-}
-
-/**
  * DOMContentLoaded
  */
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("engopro-name").innerText = ENGOPRO.NAME
     actionLink()
-    actionClick()
 });
