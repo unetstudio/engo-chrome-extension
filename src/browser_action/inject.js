e2514323798c0a79087917c99280223c79a2dda8
Duc Dao <huuduc.uneti@gmail.com>
2020-02-06 21:28:00 +0700

    add analytic

--- src/browser_action/inject.js
+++ src/browser_action/inject.js
@@ -1,4 +1,36 @@
 /**
+ * Add your Analytics tracking ID here.
+ */
+var _AnalyticsCode = 'UA-157821207-2';
+/**
+ * Below is a modified version of the Google Analytics asynchronous tracking
+ * code snippet.  It has been modified to pull the HTTPS version of ga.js
+ * instead of the default HTTP version.  It is recommended that you use this
+ * snippet instead of the standard tracking snippet provided when setting up
+ * a Google Analytics account.
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+/**
+ * Track a click on a button using the asynchronous tracking API.
+ *
+ * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
+ * for information on how to use the asynchronous tracking API.
+ */
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+}
+
+/**
  * Handle link click
  */
 function actionLink() {
@@ -12,6 +44,7 @@ function actionLink() {
                     chrome.tabs.create({active: true, url: location});
                 }
             };
+            ln.addEventListener('click', trackButtonClick);
         })();
     }
 }
